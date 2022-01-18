af6dc6788aa241d6a343ee0dd1d0e5673d791582
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-17 21:08:19 -0600

    create and delete files

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+function Navbar() {
+    return ( 
+        <div>
+            <button>Agregar Imagen</button>
+        </div>
+     );
+}
+
+export default Navbar;
