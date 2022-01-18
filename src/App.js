af813b7afb7e939bb696bdecfedf2f6c66baabe1
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-17 21:08:04 -0600

    update app with new info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+
+const search = 'mexico'
+const API_KEY = '8VzQ2kG2msZoLOMYZOSe6NsjcSXWNo5c'
+const URLAPI = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=10&offset=0&rating=g&lang=en`
 
 function App() {
+  const [gifs, setGifs] = useState([])
+
+  useEffect(function () {
+    fetch(URLAPI)
+      .then(res => res.json())
+      .then(response => {
+        const { data = [] } = response
+        const gif = data.map(image => image.images.downsized_medium.url)
+        setGifs(gif)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <section className="App-header">
+        <input />
+        <button>Buscar</button>
+        {
+          gifs.map(images => <img alt='No hay imagen' src={images} />)
+        }
+
+      </section>
     </div>
   );
 }
