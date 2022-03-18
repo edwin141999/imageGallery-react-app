06257f4bff8adc8705c23107c7b8c36e2287d867
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-03-17 23:36:40 -0600

    changes in App, delete 2 components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-
-const search = 'mexico'
-const API_KEY = '8VzQ2kG2msZoLOMYZOSe6NsjcSXWNo5c'
-const URLAPI = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=10&offset=0&rating=g&lang=en`
 
 function App() {
   const [gifs, setGifs] = useState([])
+  const [busqueda, setBusqueda] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  useEffect(function () {
-    fetch(URLAPI)
-      .then(res => res.json())
-      .then(response => {
-        const { data = [] } = response
-        const gif = data.map(image => image.images.downsized_medium.url)
-        setGifs(gif)
-      })
-  }, [])
+  const handleSubmit = () => {
+    setLoading(false)
+    const search = busqueda
+    const API_KEY = '8VzQ2kG2msZoLOMYZOSe6NsjcSXWNo5c'
+    const URLAPI = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=10&offset=0&rating=g&lang=en`
+    setTimeout(() => {
+      fetch(URLAPI)
+        .then(res => res.json())
+        .then(response => {
+          console.log(response);
+          const { data = [] } = response
+          const gif = data.map(image => image.images.downsized_medium.url)
+          setGifs(gif)
+        })
+      setLoading(true)
+    }, 100);
+  }
+
 
   return (
     <div className="App">
-      <Navbar />
       <section className="App-header">
-        <input />
-        <button>Buscar</button>
+        <input type='text' onChange={e => (setBusqueda(e.target.value))} />
+        <button onClick={handleSubmit}>Buscar</button>
         {
-          gifs.map(images => <img alt='No hay imagen' src={images} />)
+          loading
+            ?
+            gifs.map(images => <img key={images} alt='No hay imagen' src={images} />)
+            :
+            <p>Loading</p>
         }
-
       </section>
     </div>
   );
